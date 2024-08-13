6f05adf5d977b38de2b3d79f3be1d81b753aa210
Youssef El Mahallawy <youssef.elmahallawy01@gmail.com>
2024-08-13 16:57:31 -0400

    directly use image since we're not deploying to vercel anymore and can't use public folder

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,12 @@
 import { Title } from "@mantine/core";
 import Image from "next/image";
+import logo from "../../../public/logo.webp";
 
 const Header = () => {
   return (
     <header className="shadow-sm shadow-gray-500 justify-start px-6 py-4 flex gap-2">
       <Title ff="Helvetica">HeadWind</Title>
-      <Image alt="Logo preview" src={`/logo.webp`} width={45} height={45} />
+      <Image alt="Logo preview" src={logo.src} width={45} height={45} />
     </header>
   );
 };
